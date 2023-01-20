4c08f55395cf37cbb68b1e24cf489f1cea7ca179
Tianyu Huang <fjnhhty777@hotmail.com>
2023-01-20 22:33:39 +0800

    Implement Moller-Trumbore intersection algorithm.

--- src/meshobj.cpp
+++ src/meshobj.cpp
@@ -2,11 +2,8 @@
 
 #include <limits>
 
-#define CULLING
-
 namespace OpenPT
 {
-    // TODO: Implement M-T Algorithm.
     // TODO: Optimize the following using BVH and other accelerating methods.
     const bool Mesh::Intersect(const Ray &ray, float &t, float &u, float &v)
     {
@@ -16,65 +13,31 @@ namespace OpenPT
         bool flag = false;
         for (auto f : face)
         {
-            Vector3f a = geo_vert[f[0][0] - 1];
-            Vector3f b = geo_vert[f[1][0] - 1];
-            Vector3f c = geo_vert[f[2][0] - 1];
+            Vector3f v0 = geo_vert[f[0][0] - 1];
+            Vector3f v1 = geo_vert[f[1][0] - 1];
+            Vector3f v2 = geo_vert[f[2][0] - 1];
 
-            Vector3f v0v1 = b - a;
-            Vector3f v0v2 = c - a;
-            Vector3f N = v0v1.Cross(v0v2);
+            Vector3f v0v1 = v1-v0;
+            Vector3f v0v2 = v2-v0;
+            Vector3f pvec = r_d.Cross(v0v2);
+            float det = v0v1.Dot(pvec);
 
-            #ifdef CULLING
-            if (Vector3f::Dot(r_d, N) > 0)
-            {
+            if(det < EPS) continue;
+
+            Vector3f tvec = r_o - v0;
+            auto alpha = pvec.Dot(tvec);
+            if(alpha<0.0f || alpha > det) continue;
+
+            Vector3f qvec = tvec.Cross(v0v1);
+            auto beta = qvec.Dot(r_d);
+            if(beta < 0.0f || alpha+beta > det) continue;
+
+            float inv_det = 1.0f / det;
+            t = v0v2.Dot(qvec) * inv_det;
+            if(t < 0.0f){
                 continue;
             }
-            #endif
-
-            float denom = N.Dot(N);
-            float NdotRayDirection = N.Dot(r_d);
-
-            if (std::abs(NdotRayDirection) < EPS)
-            {
-                continue;
-            }
-            float d = -N.Dot(a);
-            t = -(N.Dot(r_o) + d) / NdotRayDirection;
-            if (t < 0)
-            {
-                continue;
-            }
-            Vector3f P = r_o + t * r_d;
-            Vector3f C;
-            Vector3f edge0 = b - a;
-            Vector3f vp0 = P - a;
-            C = edge0.Cross(vp0);
-            if (N.Dot(C) < 0)
-            {
-                continue;
-            }
-
-            Vector3f edge1 = c - b;
-            Vector3f vp1 = P - b;
-            C = edge1.Cross(vp1);
-            if ((u = N.Dot(C)) < 0)
-            {
-                continue;
-            }
-
-            Vector3f edge2 = a - c;
-            Vector3f vp2 = P - c;
-            C = edge2.Cross(vp2);
-            if ((v = N.Dot(C)) < 0)
-            {
-                continue;
-            }
-
-            u /= denom;
-            v /= denom;
-
             flag = true;
-            continue;
         }
         return flag;
     }
